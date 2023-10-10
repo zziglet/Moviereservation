ab6ff980e8ddce9b8f9eb7b1c5850306b5952454
SeriousBug98 <138648012+SeriousBug98@users.noreply.github.com>
2023-10-10 10:56:26 +0900

    Add files via upload

--- MovieReservation/src/member/MemberRepository.java
+++ MovieReservation/src/member/MemberRepository.java
@@ -1,7 +1,10 @@
 package member;
 
 public interface MemberRepository {
+
+    //회원가입 -> 회원 저장
     void save(Member member);
 
+    //회원조회 -> 아이디로 회원 찾자 (나중에 비밀번호로 같이 찾아야할 수 있음)
     Member findById(String id);
 }
