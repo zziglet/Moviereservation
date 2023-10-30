ba460ea69910490e0c8a8e53d41beb481d4e395e
zziglet <108401535+zziglet@users.noreply.github.com>
2023-10-30 15:34:43 +0900

    Update MovieRepository.java

--- MovieReservation/src/movie/MovieRepository.java
+++ MovieReservation/src/movie/MovieRepository.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MovieRepository {
 	
@@ -17,9 +18,11 @@ public class MovieRepository {
     	ArrayList<Movie> list = new ArrayList<Movie>();
     	boolean isInteger = false;
     	try {
+    		//movie.txt 불러오기
     		BufferedReader mvbr = new BufferedReader
     				(new InputStreamReader(new FileInputStream("./src/movie.txt"), "UTF-8"));
     		
+    		//한줄씩 읽기
     		String line;
     		while ((line = mvbr.readLine()) != null) {
     			String[] info = line.split(" ");
@@ -32,30 +35,50 @@ public class MovieRepository {
     					isInteger = false;
     				}
     				
+    				//첫 숫자가 나오기 전까지 영화제목
     				if(isInteger) {
     					String[] movienamelist = new String[i-1];
     					for(int j=1; j<i; j++) {
     						movienamelist[j-1] = info[j];
     					}
+    					
+    					//movie 인자
     					String movietheater = info[0];
     					String moviename = String.join(" ", movienamelist);
     					String moviedate = info[i];
     					String moviestarttime = info[i+1];
     					String movieendtime = info[i+2];
-    					String[] movierseat = new String[info.length - (i+3)];
-    					int cnt = 0;
-    					for(int j=i+3; j<info.length; j++) {
-    						movierseat[cnt] = info[j];
-    						cnt++;
+    					String[] movierseat = new String[72];
+    					String[] movieseat = new String[72];
+    					
+    					//좌석 초기화
+    					for(int j=0; j<12; j++) {
+    						for(int k=0; k<6; k++) {
+    							movierseat[j*6+k] = "0" + Integer.toString(k+1);
+    						}
     					}
     					
-    					Movie movie = new Movie(movietheater, moviename, moviedate, moviestarttime, movieendtime, movierseat);
+    					Arrays.fill(movieseat, "1");
+    					
+    					//예약된 좌석 표기
+    					int idx = 0;
+    					for(int j=i+3; j<info.length; j++) {
+    						String[] inforseat = info[j].split("");
+    						int temp = (int)inforseat[0].charAt(0);
+    						idx = 6*(temp - 65) + Integer.parseInt(inforseat[2]) - 1;
+    						movierseat[idx] = "00";
+    						movieseat[idx] = "0";
+    					}
+    					
+    					Movie movie = new Movie(movietheater, moviename, moviedate, moviestarttime, movieendtime, movierseat, movieseat);
 						
-    					/* 구현 도중 확인 출력 문구인데 나중에 없애도 될까요
+
+						/*
 						 * System.out.printf("%s %s %s %s %s", movie.getTheater(), movie.getName(),
 						 * movie.getDate(), movie.getStart(), movie.getEnd()); for(String st :
 						 * movie.getRseat()) { System.out.printf(" %s", st); } System.out.println();
 						 */
+						
     					
     					list.add(movie);
     					break;
@@ -77,4 +100,3 @@ public class MovieRepository {
     }
 
 }
-
