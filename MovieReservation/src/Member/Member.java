775f6d43ef0351c5fed59857060d7dd9d02473ae
unhee2 <dldnsgml7@naver.com>
2023-11-01 17:36:15 +0900

    최종이었으면좋겠다!

--- MovieReservation/src/Member/Member.java
+++ MovieReservation/src/Member/Member.java
@@ -14,7 +14,7 @@ public class Member {
     private String pw;
     private String name;
 
-    private ArrayList<Movie> movieList;
+    private final ArrayList<Movie> movieList;
 
     public Member(String id, String pw, String name) {
         this.id = id;
@@ -26,7 +26,7 @@ public class Member {
     public Member() {
         this.id = "";
         this.pw = "";
-        this.name= "";
+        this.name = "";
         this.movieList = new ArrayList<>();
     }
 
@@ -54,53 +54,52 @@ public class Member {
         this.name = name;
     }
 
-    public ArrayList<Movie> getMovielist(){
+    public ArrayList<Movie> getMovielist() {
         // txt 파일을 읽어와서 movieList로 반환
         boolean isInteger;
         try {
+            String userdir = System.getProperty("user.dir") + "/MovieReservation/src/user/";
+            String path = userdir + id + ".txt";
             BufferedReader membr = new BufferedReader
                     (new InputStreamReader
-                            (new FileInputStream("./src/user/"+this.id+".txt"), StandardCharsets.UTF_8));
+                            (new FileInputStream(path), StandardCharsets.UTF_8));
 
             String line;
             membr.readLine(); // -> 첫 줄에는 계정 정보가 있으므로 넘김
             while ((line = membr.readLine()) != null) {
                 String[] info = line.split(" ");
-                for(int i=1 ; i<info.length; i++) {
+                for (int i = 1; i < info.length; i++) {
                     String[] info2 = info[i].split("");
-                    try{
+                    try {
                         Integer.parseInt(info2[0]);
                         isInteger = true;
-                    }catch(NumberFormatException e) {
+                    } catch (NumberFormatException e) {
                         isInteger = false;
                     }
 
-                    if(isInteger) {
-                        String[] movienamelist = new String[i-1];
-                        for(int j=1; j<i; j++) {
-                            movienamelist[j-1] = info[j];
-                        }
+                    if (isInteger) {
+                        String[] movienamelist = new String[i - 1];
+                        System.arraycopy(info, 1, movienamelist, 0, i - 1);
 
                         String movietheater = info[0];
                         String moviename = String.join(" ", movienamelist);
                         String moviedate = info[i];
-                        String moviestarttime = info[i+1];
-                        String movieendtime = info[i+2];
-                        String[] movierseat = new String[info.length - (i+4)];
+                        String moviestarttime = info[i + 1];
+                        String movieendtime = info[i + 2];
+                        String[] movierseat = new String[info.length - (i + 4)];
                         String[] movieseat = {""};
                         int cnt = 0;
-                        for(int j=i+4; j<info.length; j++) {
+                        for (int j = i + 4; j < info.length; j++) {
                             movierseat[cnt] = info[j];
                             cnt++;
                         }
 
-                        Movie movie = new Movie(movietheater, moviename, moviedate, moviestarttime, movieendtime, movierseat,movieseat);
+                        Movie movie = new Movie(movietheater, moviename, moviedate, moviestarttime, movieendtime, movierseat, movieseat);
                         System.out.printf("%s %s %s %s %s", movie.getTheater(), movie.getName(),
                                 movie.getDate(), movie.getStart(), movie.getEnd());
                         System.out.println();
                         System.out.println("===================================");
-                        for(String st : movie.getRseat())
-                        {
+                        for (String st : movie.getRseat()) {
                             System.out.printf(" %s", st);
                         }
                         System.out.println();
@@ -118,7 +117,7 @@ public class Member {
                 }
             }
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
--- MovieReservation/src/Member/MemberService.java
+++ MovieReservation/src/Member/MemberService.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class MemberService {
 
     //영화 예매를 위한 메소드 (인자를 뭘 넘겨야할지 아직 감이 안옴)
-    void CreateReservation(Member member, Movie movie){
+    public void CreateReservation(Member member){
         
     }
 
