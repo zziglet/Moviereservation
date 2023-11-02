acb571a13cdaba5dd7b3fd93fb7b68120fc09fc5
HwiWon Cho <118262343+hwidung@users.noreply.github.com>
2023-11-02 13:21:21 +0900

    Update MemberService.java CancelReservation

--- MovieReservation/src/Member/MemberService.java
+++ MovieReservation/src/Member/MemberService.java
@@ -165,43 +165,47 @@ public class MemberService {
 
     //영화 예매 취소 메소드
     public void CancelReservation(Member member) {
-        System.out.print("[예매 취소]\n사용자의 예매 내역입니다.\n취소할 예매 내역을 골라주세요.");
+        System.out.print("[예매 취소]\n사용자의 예매 내역입니다.\n취소할 예매 내역을 골라주세요.\n");
         //예매 목록 출력 id.txt
-        MemberRepository repository=new MemberRepository();
-        repository.FindMovie(member);
+        memberRepository.FindMovie(member);
+        MainMenu mainmenu=new MainMenu(member);
 
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        while(true){
-            int choice=scanner.nextInt();
-            ArrayList<Movie> movieList=member.getMovielist();
+        while (true) {
+            System.out.print("MovieReservation >> ");
+            int choice = scanner.nextInt();
+            ArrayList<Movie> movieList = member.getMovielist();
 
-            if(choice>0&&choice<=movieList.size()){
-                Movie cancledMovie=movieList.get(choice-1);
-                System.out.println("["+choice+"]");
-                System.out.println("영화 제목:"+cancledMovie.getName());
-                System.out.println("상영 날짜:"+cancledMovie.getDate());
-                System.out.println("좌석:"+String.join("/", cancledMovie.getRseat()));
+            if (choice > 0 && choice <= movieList.size()) {
+                Movie cancelMovie = movieList.get(choice - 1);
+                System.out.println("[" + choice + "]");
+                System.out.println("영화 제목:" + cancelMovie.getName());
+                System.out.println("상영 날짜:" + cancelMovie.getDate());
+                System.out.println("좌석:" + String.join("/", cancelMovie.getRseat()));
 
-                while(true){
-                    System.out.println("예매를 취소하시겠습니까? (y/n): ");
-                    String ans=scanner.next();
-                    if(ans.equals("y")||ans.equals("Y")) {
-                        System.out.println("예매가 취소되었습니다.");
-                        //repository.DeleteMembertxtMovie();
-                        //repository.DeleteMovietxt();
-                        break;
-                    }else if(ans.equals("n")||ans.equals("N")) {
-                        System.out.println("메인 메뉴로 돌아갑니다.");
-                        MainMenu mainmenu = new MainMenu();
+                System.out.println("예매를 취소하시겠습니까? (y/n): ");
+                while (true) {
+                    System.out.print("MovieReservation >> ");
+                    String ans = scanner.next();
+                    if (ans.equals("y") || ans.equals("Y")) {
+                        System.out.println("사용자의 예매 내역이 취소되었습니다. 메인 메뉴로 돌아갑니다.");
+                        for (String seat : cancelMovie.getRseat()) {
+                            memberRepository.DeleteMembertxtMovie(cancelMovie, member);
+                            movieRepository.DeleteMovietxt(cancelMovie, seat);
+                        }
                         mainmenu.ShowMenu();
                         break;
-                    }else{
+                    } else if (ans.equals("n") || ans.equals("N")) {
+                        System.out.println("메인 메뉴로 돌아갑니다.");
+                        mainmenu.ShowMenu();
+                        break;
+                    } else {
                         System.out.println("..! 오류: 예매 취소를 원하시면 y를 원하지 않으시면 n을 입력해야합니다. 다시 입력해주세요.");
                     }
                 }
                 break;
-            }else{
+            } else {
                 System.out.println("..! 오류: 예매되어 있는 영화 번호를 입력해야 합니다. 다시 입력해주세요");
             }
         }
