f512e5c716e3ecdccf798158046bf73da0f6d1f0
zziglet <108401535+zziglet@users.noreply.github.com>
2023-11-06 02:28:10 +0900

    Add files via upload

---
+++ src/Menu/Main.java
@@ -0,0 +1,10 @@
+package Menu;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SignUpInMenu SignUpInMenu = new SignUpInMenu();
+        SignUpInMenu.Start();
+    }
+}
---
+++ src/Menu/MainMenu.java
@@ -0,0 +1,51 @@
+package Menu;
+
+import Member.*;
+import java.util.Scanner;
+import static java.lang.System.exit;
+
+public class MainMenu {
+    Member member= new Member();
+    
+	public MainMenu(Member member) {
+		super();
+		this.member = member;
+	}
+	
+    public void ShowMenu(){
+        try (Scanner scan = new Scanner(System.in)) {
+			MemberService memberservice = new MemberService();
+			MyPageMenu mypagemenu = new MyPageMenu(this.member);
+			
+
+
+
+			while(true){
+			    System.out.print("[메인 메뉴] 원하시는 서비스의 숫자를 입력해주세요.\n\n");
+			    System.out.print("  1. 예매\n  2. 마이페이지\n  3. 종료\n");
+			    System.out.print("MovieReservation >>");
+			    
+			    String input=scan.nextLine();
+			    String result = input.replaceAll("\\s+", "");
+			    
+			    if(result.equals("1"))
+			    	memberservice.CreateReservation(this.member);
+			    else if(result.equals("2"))
+			    	mypagemenu.ShowMenu2();
+			    else if(result.equals("3"))
+			    	Exit();
+			    else {
+			        System.out.print("..! 오류 : 잘못된 입력입니다. 다시 입력해주세요");
+			        System.out.println();
+			    }
+			}
+		}
+
+    }
+
+    public void Exit(){
+        System.out.println("영화 예매 프로그램을 이용해주셔서 감사합니다.\n프로그램을 종료합니다.\n");
+        exit(0);
+    }
+
+}
