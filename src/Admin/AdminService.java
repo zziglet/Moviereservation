655c3a48706c02cbe89807b8176f3922c68b6da6
unhee2 <dldnsgml7@naver.com>
2023-11-28 00:23:12 +0900

    Admin 초기 원판

---
+++ src/Admin/AdminService.java
@@ -0,0 +1,16 @@
+package Admin;
+
+public class AdminService {
+    public void EditTheaterNum(String keyString, String repString){
+        
+    }
+    public void EditTheaterSeat(String keyString, int seatNum){
+        
+    }
+    public void EditTheaterInfoName(String keyString, String moviename){
+        
+    }
+    public void EditTheaterRuntime(String keyString, int runningTime){
+        
+    }
+}
