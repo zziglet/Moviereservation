e9299eb14baaa3797a02860fd18f3f5811bca588
unhee2 <dldnsgml7@naver.com>
2023-11-30 16:22:34 +0900

    editSeats 어쩌구 수정

--- src/Admin/AdminService.java
+++ src/Admin/AdminService.java
@@ -1,8 +1,10 @@
 package Admin;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +13,7 @@ import java.util.Scanner;
 
 public class AdminService {
     public void EditTheaterNum() {
+        /* 관번호 수정 */
         try {
             // 1.
             // 관리자에게 수정할 관 번호 선택
@@ -71,9 +74,7 @@ public class AdminService {
             fileout.close();
             scan.close();
 
-            //////////////////////////////////////////////////////////
-            //// movie.txt (상영 스케줄)에 수정된 내역 적용
-
+            /* movie.txt (상영 스케줄)에 수정된 내역 적용 */
             path = srcdir + "movie.txt";
             membr = new BufferedReader(
                     new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
@@ -101,7 +102,7 @@ public class AdminService {
 
             // 예매 내역 반영
 
-            path = userdir + "movie.txt";
+            path = userdir + "movie.txt"; // 바꿔야함
             membr = new BufferedReader(
                     new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
             while ((line = membr.readLine()) != null) {
@@ -126,8 +127,111 @@ public class AdminService {
         }
     }
 
-    public void EditTheaterSeat(String keyString, int seatNum) {
+    public void EditTheaterSeat() {
+        /* 전체 좌석 수 변경 -> 예매 내역 제외 후 반영하기 */
+        String userdir = System.getProperty("user.dir") + "./src/user/";
+        String srcdir = System.getProperty("user.dir") + "./src/";
+        try {
 
+            /* 1. 제외시킬 내역을 배열에 담기 */
+
+            File folder = new File(userdir);
+            File[] filelist = folder.listFiles();
+            ArrayList<String> tkeyinIDList = new ArrayList<>();
+            String[] info;
+            String line;
+            // id.txt에서 예매가 된 내역의 Theater 키값 추출
+            if (filelist != null) {
+                for (File file : filelist) {
+                    if (file.isFile() && file.canRead()) {
+                        FileReader filereader = new FileReader(file);
+                        BufferedReader bufReader = new BufferedReader(filereader);
+
+                        // id.txt 속의 예매된 theater 키값 추출
+                        info = null;
+                        while ((line = bufReader.readLine()) != null) {
+                            info = line.split(" ");
+                            if (info[0].matches("^[M-Z][\\d]+$")) {
+                                tkeyinIDList.add(info[1]);
+                            }
+                        }
+                        bufReader.close();
+                    }
+                }
+            }
+            // 중복제거
+            for (String item : tkeyinIDList) {
+                if (!tkeyinIDList.contains(item))
+                    tkeyinIDList.add(item);
+            }
+
+            /* 2. theater.txt에 저장되어있는 키값 추출 */
+            String path = srcdir + "theater.txt";
+            BufferedReader bufReader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+            
+            ArrayList<String> theaterList = new ArrayList<>();
+            while ((line = bufReader.readLine()) != null) {
+                theaterList.add(line);
+            }
+            bufReader.close();
+
+            /* 3. 각 값을 비교해 같으면 제외(삭제) */
+            info = null;
+            for(String str : theaterList){
+                info = str.split(" ");
+                if(tkeyinIDList.contains(info[0])){
+                    theaterList.remove(str);
+                }
+            }
+            
+            /* 4. 제외된 내역 출력 */
+            System.out.println("수정할 좌석 수가 담긴 리스트입니다. \n");
+            for(int i =0; i < theaterList.size();i++){
+                System.out.println((i+1)+": "+theaterList.get(i));
+            }
+            System.out.println("수정할 리스트의 번호를 입력하십시오.");
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+            // 오류 처리 해줘야함
+            int result = Integer.parseInt(input.replaceAll("\\s+", ""));
+
+            System.out.println("수정할 좌석 수를 입력하십시오.");
+            input = scan.nextLine();
+            // 오류 처리 해줘야함
+            String seatsNum = input.replaceAll("\\s+", "");
+            info = theaterList.get(result-1).split(" ");
+            String theaterKey = info[0];
+
+            // theater.txt에 수정된 내역 적용
+
+            path = srcdir + "theater.txt";
+            bufReader = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+            StringBuffer inputBuffer = new StringBuffer();
+            
+            while ((line = bufReader.readLine()) != null) {
+                info = line.split(" ");
+                if (info[0].equals(theaterKey))
+                    info[2] = seatsNum;
+                line = "";
+                for (String str : info) {
+                    line += str + " ";
+                }
+                inputBuffer.append(line);
+                inputBuffer.append("\n");
+            }
+
+            bufReader.close();
+
+            FileOutputStream fileout = new FileOutputStream(path);
+            fileout.write(inputBuffer.toString().getBytes());
+            fileout.close();
+            scan.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void EditTheaterInfoName(String keyString, String moviename) {
--- src/Menu/Main.java
+++ src/Menu/Main.java
@@ -1,10 +1,13 @@
 package Menu;
 
+import Admin.AdminService;
 
 public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        SignUpInMenu SignUpInMenu = new SignUpInMenu();
-        SignUpInMenu.Start();
+        // SignUpInMenu SignUpInMenu = new SignUpInMenu();
+        // SignUpInMenu.Start();
+        AdminService adminService = new AdminService();
+        adminService.EditTheaterNum();
     }
 }
