5d4467a22a9ffdb909732ca462a967834bbcf831
hwidung <hwhwhw74@naver.com>
2023-12-03 04:47:27 +0900

    EditTheaterInfoName() 완료

--- src/Admin/AdminService.java
+++ src/Admin/AdminService.java
@@ -859,169 +859,205 @@ public class AdminService {
             System.out.println();
             System.out.println("수정할 리스트의 번호를 입력하십시오.\n");
             Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            // 오류 처리 해줘야함
-            int result = Integer.parseInt(input.replaceAll("\\s+", ""));
-            System.out.println("수정할 제목을 입력하십시오.");
-            input = scan.nextLine();
 
-            // 오류 처리 해줘야함
-            String repString = input;
-            String[] info = movieList.get(result - 1).split(" ");
-            String movieinfoKey = info[0];
-
-            /// movieinfo.txt 에 수정된 내역 적용
-            path = srcdir + "movieinfo.txt";
-            membr = new BufferedReader(
-                    new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
-            FileOutputStream fileout;
-            while ((line = membr.readLine()) != null) {
-                if (info.length == 0) {
-                    break;
+            boolean flag1=false;
+            lp1:
+            while(!flag1){
+                System.out.print("MovieReservation >> ");
+                String input = scan.nextLine();
+                // 오류 처리 해줘야함
+                if(!Pattern.matches("^[1-9][0-9]?$", input)){
+                    System.out.println("..! 오류 : 잘못된 입력입니다. 다시 입력해주세요.\n");
+                    continue lp1;
                 }
-                info = line.split(" ");
-                String[] temp = new String[3];
-                // movieinfo.txt의 key, 제목, running time을 담는 배열
-                temp[0] = info[0];
-                temp[2] = info[info.length - 1]; // 마지막 방에는 running time이 담겨 있음
-                String tempString = "";
-
-                for (int i = 1; i < info.length - 1; i++) {
-                    tempString += info[i] + " ";
+                int result = Integer.parseInt(input.replaceAll("\\s+", ""));
+                if (result < 1 || result > movieList.size()) {
+                    System.out.println("..! 오류 : 존재하지 않는 리스트입니다. 다시 입력해주세요.\n");
+                    continue lp1;
                 }
-                tempString = tempString.substring(0, tempString.length() - 1);
-                temp[1] = tempString;
+                flag1=true;
+                System.out.println();
+                System.out.println("수정할 제목을 입력하십시오.");
+                boolean flag2=false;
+                lp2:
+                while(!flag2){
+                    System.out.print("MovieReservation >> ");
+                    input = scan.nextLine();
 
-                if (temp[0].equals(movieinfoKey))
-                    temp[1] = repString;
-
-                line = "";
-                for (String str : temp) {
-                    line += str + " ";
-                }
-                inputBuffer.append(line);
-                inputBuffer.append("\n");
-
-
-            }
-            membr.close();
-            fileout = new FileOutputStream(path);
-            fileout.write(inputBuffer.toString().getBytes());
-            fileout.close();
-            inputBuffer.delete(0, inputBuffer.length());
-            //// movie.txt (상영 스케줄)에 수정된 내역 적용
-            path = srcdir + "movie.txt";
-            membr = new BufferedReader(
-                    new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
-
-            while ((line = membr.readLine()) != null) {
-                info = line.split(" ");
-                if (info.length == 0) {
-                    break;
-                }
-                int dateIndex = 0; // info 배열에서 날짜를 담은 방의 번호 -> 상영관 이후 ~ 날짜 전까지는 전부 제목으로 하기 위함
-
-                for (int i = 0; i < info.length; i++) {
-                    if (Pattern.matches("^[\\d]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", info[i])) {
-                        dateIndex = i;
+                    // 오류 처리 해줘야함
+                    if(!Pattern.matches("^[가-힣a-zA-Z0-9 ,:.!?()~-]+$", input)){
+                        System.out.println("..! 오류 : 잘못된 입력입니다. 다시 입력해주세요.\n");
+                        continue lp1;
                     }
-                }
-                ArrayList<String> temp = new ArrayList<>();
+                    String repString = input;
+                    String[] info = movieList.get(result - 1).split(" ");
+                    String movieinfoKey = info[0];
 
-                for (int i = 0; i < 4; i++) {
-                    temp.add(info[i]);
-                    // tempStrings[]에 기존 키값들, 상영관 복사
-                }
+                    String movieName=info[1];
+                    if (movieName.equals(repString)) {
+                        System.out.println("..! 오류 : 입력된 영화 제목은 기존의 영화 제목과 동일합니다. 다시 입력해주세요.\n");
+                        continue lp2;
+                    }
+                    flag2=true;
+                    System.out.println("수정이 완료되었습니다.\n");
 
-                String tempString = "";
+                    /// movieinfo.txt 에 수정된 내역 적용
+                    path = srcdir + "movieinfo.txt";
+                    membr = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+                    FileOutputStream fileout;
+                    while ((line = membr.readLine()) != null) {
+                        if (info.length == 0) {
+                            break;
+                        }
+                        info = line.split(" ");
+                        String[] temp = new String[3];
+                        // movieinfo.txt의 key, 제목, running time을 담는 배열
+                        temp[0] = info[0];
+                        temp[2] = info[info.length - 1]; // 마지막 방에는 running time이 담겨 있음
+                        String tempString = "";
 
-                for (int i = 4; i < dateIndex; i++) {
-                    tempString += info[i] + " ";
-                }
-                tempString = tempString.substring(0, tempString.length() - 1);
+                        for (int i = 1; i < info.length - 1; i++) {
+                            tempString += info[i] + " ";
+                        }
+                        tempString = tempString.substring(0, tempString.length() - 1);
+                        temp[1] = tempString;
 
-                if (temp.get(2).equals(movieinfoKey))
-                    temp.add(repString);
-                else
-                    temp.add(tempString);
+                        if (temp[0].equals(movieinfoKey))
+                            temp[1] = repString;
 
-
-                for (int i = dateIndex; i < info.length; i++)
-                    temp.add(info[i]);
-
-                line = "";
-                for (String str : temp) {
-                    line += str + " ";
-                }
-                inputBuffer.append(line);
-                inputBuffer.append("\n");
-            }
-            membr.close();
-            fileout = new FileOutputStream(path);
-            fileout.write(inputBuffer.toString().getBytes());
-            fileout.close();
-
-            inputBuffer.delete(0, inputBuffer.length());
-
-            // 예매 내역 반영
-            File folder = new File(userdir);
-            File[] filelist = folder.listFiles();
-            if (filelist != null) {
-                for (File file : filelist) {
-                    if (file.isFile() && file.canRead()) {
-                        FileReader filereader = new FileReader(file);
-                        BufferedReader bufReader = new BufferedReader(filereader);
-                        String admin = bufReader.readLine();
-                        inputBuffer.delete(0, inputBuffer.length());
-                        inputBuffer.append(admin);
+                        line = "";
+                        for (String str : temp) {
+                            line += str + " ";
+                        }
+                        inputBuffer.append(line);
                         inputBuffer.append("\n");
-                        // 계정 정보 넘김
-                        while ((line = bufReader.readLine()) != null) {
 
-                            info = line.split(" ");
-                            int dateIndex = 0; // info 배열에서 날짜를 담은 방의 번호 -> 상영관 이후 ~ 날짜 전까지는 전부 제목으로 하기 위함
 
-                            for (int i = 0; i < info.length; i++) {
-                                if (Pattern.matches("^[\\d]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", info[i])) {
-                                    dateIndex = i;
-                                }
+                    }
+                    membr.close();
+                    fileout = new FileOutputStream(path);
+                    fileout.write(inputBuffer.toString().getBytes());
+                    fileout.close();
+                    inputBuffer.delete(0, inputBuffer.length());
+                    //// movie.txt (상영 스케줄)에 수정된 내역 적용
+                    path = srcdir + "movie.txt";
+                    membr = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+
+                    while ((line = membr.readLine()) != null) {
+                        info = line.split(" ");
+                        if (info.length == 0) {
+                            break;
+                        }
+                        int dateIndex = 0; // info 배열에서 날짜를 담은 방의 번호 -> 상영관 이후 ~ 날짜 전까지는 전부 제목으로 하기 위함
+
+                        for (int i = 0; i < info.length; i++) {
+                            if (Pattern.matches("^[\\d]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", info[i])) {
+                                dateIndex = i;
                             }
-                            ArrayList<String> temp = new ArrayList<>();
+                        }
+                        ArrayList<String> temp = new ArrayList<>();
 
-                            for (int i = 0; i < 4; i++) {
-                                temp.add(info[i]);
-                                // tempStrings[]에 기존 키값들, 상영관 복사
-                            }
-
-                            String tempString = "";
-
-                            for (int i = 4; i < dateIndex; i++) {
-                                tempString += info[i] + " ";
-                            }
-                            tempString = tempString.substring(0, tempString.length() - 1);
-
-                            if (temp.get(2).equals(movieinfoKey))
-                                temp.add(repString);
-                            else
-                                temp.add(tempString);
-
-                            for (int i = dateIndex; i < info.length; i++)
-                                temp.add(info[i]);
-
-                            line = "";
-                            for (String str : temp) {
-                                line += str + " ";
-                            }
-                            inputBuffer.append(line);
-                            inputBuffer.append("\n");
+                        for (int i = 0; i < 4; i++) {
+                            temp.add(info[i]);
+                            // tempStrings[]에 기존 키값들, 상영관 복사
                         }
 
-                        bufReader.close();
-                        fileout = new FileOutputStream(file);
-                        fileout.write(inputBuffer.toString().getBytes());
-                        fileout.close();
+                        String tempString = "";
+
+                        for (int i = 4; i < dateIndex; i++) {
+                            tempString += info[i] + " ";
+                        }
+                        tempString = tempString.substring(0, tempString.length() - 1);
+
+                        if (temp.get(2).equals(movieinfoKey))
+                            temp.add(repString);
+                        else
+                            temp.add(tempString);
+
+
+                        for (int i = dateIndex; i < info.length; i++)
+                            temp.add(info[i]);
+
+                        line = "";
+                        for (String str : temp) {
+                            line += str + " ";
+                        }
+                        inputBuffer.append(line);
+                        inputBuffer.append("\n");
+                    }
+                    membr.close();
+                    fileout = new FileOutputStream(path);
+                    fileout.write(inputBuffer.toString().getBytes());
+                    fileout.close();
+
+                    inputBuffer.delete(0, inputBuffer.length());
+
+                    // 예매 내역 반영
+                    File folder = new File(userdir);
+                    File[] filelist = folder.listFiles();
+                    if (filelist != null) {
+                        for (File file : filelist) {
+                            if (file.isFile() && file.canRead()) {
+                                FileReader filereader = new FileReader(file);
+                                BufferedReader bufReader = new BufferedReader(filereader);
+                                String admin = bufReader.readLine();
+                                inputBuffer.delete(0, inputBuffer.length());
+                                inputBuffer.append(admin);
+                                inputBuffer.append("\n");
+                                // 계정 정보 넘김
+                                while ((line = bufReader.readLine()) != null) {
+
+                                    info = line.split(" ");
+                                    int dateIndex = 0; // info 배열에서 날짜를 담은 방의 번호 -> 상영관 이후 ~ 날짜 전까지는 전부 제목으로 하기 위함
+
+                                    for (int i = 0; i < info.length; i++) {
+                                        if (Pattern.matches("^[\\d]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", info[i])) {
+                                            dateIndex = i;
+                                        }
+                                    }
+                                    ArrayList<String> temp = new ArrayList<>();
+
+                                    for (int i = 0; i < 4; i++) {
+                                        temp.add(info[i]);
+                                        // tempStrings[]에 기존 키값들, 상영관 복사
+                                    }
+
+                                    String tempString = "";
+
+                                    for (int i = 4; i < dateIndex; i++) {
+                                        tempString += info[i] + " ";
+                                    }
+                                    tempString = tempString.substring(0, tempString.length() - 1);
+
+                                    if (temp.get(2).equals(movieinfoKey))
+                                        temp.add(repString);
+                                    else
+                                        temp.add(tempString);
+
+                                    for (int i = dateIndex; i < info.length; i++)
+                                        temp.add(info[i]);
+
+                                    line = "";
+                                    for (String str : temp) {
+                                        line += str + " ";
+                                    }
+                                    inputBuffer.append(line);
+                                    inputBuffer.append("\n");
+                                }
+
+                                bufReader.close();
+                                fileout = new FileOutputStream(file);
+                                fileout.write(inputBuffer.toString().getBytes());
+                                fileout.close();
+                            }
+                        }
                     }
                 }
+                System.out.println("관리자 메뉴로 돌아갑니다.\n");
+                AdminMenu showAdminMenu=new AdminMenu();
+                showAdminMenu.ShowAdminMenu();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -1072,7 +1108,7 @@ public class AdminService {
             /* 4. 제외된 내역 출력 */
             System.out.println("수정할 좌석 수가 담긴 리스트입니다. \n");
             for (int i = 0; i < runningList.size(); i++) {
-                System.out.println((i + 1) + ": " + runningList.get(i));
+                System.out.println((i + 1) + ". " + runningList.get(i));
             }
             System.out.println("수정할 리스트의 번호를 입력하십시오.");
             Scanner scan = new Scanner(System.in);
